15242ac3bc6573d06d05d13162f6abe04f9f6c68
Tommi <surfing@tommi.space>
2022-12-04 11:01:16 +0100

    encoding Gemini urls

--- titan/titan.sh
+++ titan/titan.sh
@@ -12,8 +12,9 @@ for f in *.md; do
 	filename=$(basename -- "$f")
 	extension="${filename##*.}"
 	filename="${filename%.*}"
+	encoded_filename=`urlencode "$filename"`
 	md2gemini "$f" -w -f -p -l copy -m -b "$BASE_URL";
-	echo -e "=> $BASE_URL/$filename.gmi $filename" >> /tmp/tommi.space/gemini/zibaldone.gmi;
+	echo -e "=> $BASE_URL/$encoded_filename.gmi $filename" >> /tmp/tommi.space/gemini/zibaldone.gmi;
 done
 sudo mv /home/admin/tommi.space/content/posts/*.gmi /opt/yunohost/gemserv/tommi.space/ -vf;
 echo 'Zibaldone has been parsed.';
@@ -23,8 +24,9 @@ for f in *.md; do
 	filename=$(basename -- "$f")
 	extension="${filename##*.}"
 	filename="${filename%.*}"
+	encoded_filename=`urlencode "$filename"`
 	md2gemini "$f" -w -f -p -l copy -m -b 'https://tommi.space/';
-	echo -e "=> $BASE_URL/$filename.gmi $filename" >> /tmp/tommi.space/gemini/jam.gmi;
+	echo -e "=> $BASE_URL/$encoded_filename.gmi $filename" >> /tmp/tommi.space/gemini/jam.gmi;
 done
 sudo mv /home/admin/tommi.space/content/notes/public/*.gmi /opt/yunohost/gemserv/tommi.space/ -vf;
 echo 'The Jam has been parsed.';
