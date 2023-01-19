b0a6d48db4a4581abde5eb4ed4fc2c96fb54048c
Tommi <surfing@tommi.space>
2023-01-19 20:06:03 +0100

    testing rsync (#2)
    Signed-off-by: Tommi <surfing@tommi.space>

--- bang/bang.sh
+++ bang/bang.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
 cd ~/tommi.space;
-git push --recurse-submodules=on-demand;
-ssh server 'boom';
+npm run build && \
+rsync -avzP -e 'ssh -p 50002' --delete ~/tommi.space/www/ admin@server.tommi.space:/home/admin/www.tmp/ && \
+ssh server 'sudo rm -rf /var/www/my_webapp/www && sudo cp -rvf /home/admin/www.tmp/ /var/www/my_webapp/www' && \
+echo -e "\033[1;32mSUCCESS\!\033[0m";
